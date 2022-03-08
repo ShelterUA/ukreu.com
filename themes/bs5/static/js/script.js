2f2602552809d10bc0a500e8d1524ebc5675ab6a
Aleksandr Turchenyuck <a.turchenyuck@clearjunction.com>
2022-03-08 20:38:10 +0100

    fixed mobile menu

--- themes/bs5/static/js/script.js
+++ themes/bs5/static/js/script.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
     let mobile_menu = $(".mobile-menu");
     let mobile_menu_btn = $(".mobile-menu-btn");
 
+    $(".mobile-menu a").click(function() {
+        if(mobile_menu.hasClass("open")) {
+            mobile_menu.animate({"left": "-320px"}, 1000, function () {
+                $(this).removeClass("open");
+            });
+        }
+    });
+
     mobile_menu_btn.click(function() {
         if(!mobile_menu.hasClass("open")) {
             mobile_menu.animate({"left" : "0px"}, 1000, function() {
@@ -28,5 +36,5 @@ $(document).ready(function() {
                 mobile_menu_btn.removeClass("fixed");
             }
         }
-    })
+    });
 });
